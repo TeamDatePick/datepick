087e5c656eea30960af078fe0122c01a4c8ff68e
narustudio <slackjawed12@gmail.com>
2024-02-04 01:03:14 +0900

    feature(api): config 파일 경로 수정 및 파일 생성

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './default/app.controller';
 import { AppService } from './default/app.service';
 import { LocationModule } from './location/location.module';
 
 @Module({
-  imports: [LocationModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [
+        '../env/.env.development',
+        '../env/.env.production',
+        '../env/.env.test',
+      ],
+    }),
+    LocationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
