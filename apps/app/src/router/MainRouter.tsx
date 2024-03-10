af5ea2c154c325f9994c91578e0d76f720c7be52
narustudio <slackjawed12@gmail.com>
2024-03-10 18:08:26 +0900

    feature(app): main router image import and header hide

--- apps/app/src/router/MainRouter.tsx
+++ apps/app/src/router/MainRouter.tsx
@@ -2,16 +2,25 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import BookmarkScreen from '../screens/bookmark/BookmarkScreen';
 import HomeScreen from '../screens/home/HomeScreen';
 import MoreScreen from '../screens/more/MoreScreen';
-
+import {Image, ImageSourcePropType} from 'react-native';
+import BookmarkTabImage from '../assets/images/bottomtab/bottomtab-bookmark.png';
+import HomeTabImage from '../assets/images/bottomtab/bottomtab-home.png';
+import MoreTabImage from '../assets/images/bottomtab/bottomtab-more.png';
 const Tab = createBottomTabNavigator();
 
 export const MainRouter = () => {
   return (
-    <Tab.Navigator initialRouteName="Home">
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+      }}>
       <Tab.Screen
         name="HomeScreen"
         component={HomeScreen}
         options={{
+          tabBarIcon: ({focused}) =>
+            BottomTabImage({focused, imageSource: HomeTabImage}),
           tabBarLabel: '홈',
         }}
       />
@@ -19,6 +28,8 @@ export const MainRouter = () => {
         name="BookmarkScreen"
         component={BookmarkScreen}
         options={{
+          tabBarIcon: ({focused}) =>
+            BottomTabImage({focused, imageSource: BookmarkTabImage}),
           tabBarLabel: '북마크',
         }}
       />
@@ -26,10 +37,32 @@ export const MainRouter = () => {
         name="MoreScreen"
         component={MoreScreen}
         options={{
+          tabBarIcon: ({focused}) =>
+            BottomTabImage({focused, imageSource: MoreTabImage}),
           tabBarLabel: '더보기',
         }}
       />
     </Tab.Navigator>
+  );
+};
+
+const BottomTabImage = ({
+  focused,
+  imageSource,
+}: {
+  focused: boolean;
+  imageSource: ImageSourcePropType;
+}) => {
+  return (
+    <Image
+      source={imageSource}
+      resizeMode="contain"
+      style={{
+        width: 25,
+        height: 25,
+        tintColor: focused ? '#000000' : '#CCCBCB',
+      }}
+    />
   );
 };
 
