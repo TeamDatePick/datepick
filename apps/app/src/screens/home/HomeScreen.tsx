431cf7ac67d9d73b6c6210db7918ab96c54e1ebc
narustudio <slackjawed12@gmail.com>
2024-03-10 18:08:44 +0900

    feature(app): add home screen test version

--- apps/app/src/screens/home/HomeScreen.tsx
+++ apps/app/src/screens/home/HomeScreen.tsx
@@ -1,7 +1,29 @@
-import {View} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, useColorScheme} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 export const HomeScreen = () => {
-  return <View></View>;
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView style={styles(isDarkMode).safeAreaView}>
+      <Text>this is home screen</Text>
+    </SafeAreaView>
+  );
 };
+
+const styles = (isDarkMode: boolean) =>
+  StyleSheet.create({
+    safeAreaView: {
+      backgroundColor: isDarkMode ? Colors.dark : Colors.white,
+      flex: 1,
+    },
+    normal: {
+      backgroundColor: 'orange',
+      height: 100,
+    },
+    button: {
+      backgroundColor: '#efefef',
+    },
+  });
 
 export default HomeScreen;
